7ef1ef3b913d26d7a29d144f96ab3d74d6f9d795
Daddywang776 <md6048036@gmail.com>
2026-07-18 00:33:17 +0530

    Fix import statement for R resource

--- app/src/main/java/eu/kanade/tachiyomi/ui/webview/WebViewActivity.kt
+++ app/src/main/java/eu/kanade/tachiyomi/ui/webview/WebViewActivity.kt
@@ -8,7 +8,7 @@ import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.core.net.toUri
-import com.Takumi.aurora.R
+import eu.kanade.tachiyomi.R
 import eu.kanade.presentation.webview.WebViewScreenContent
 import eu.kanade.tachiyomi.animesource.online.AnimeHttpSource
 import eu.kanade.tachiyomi.network.NetworkHelper
